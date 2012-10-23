02b8964bf1da0c279d664dc11dc50f8f9eda1060
durvalp1@gmail.com <durvalp1@gmail.com@89bed933-a9be-4298-65f6-25807a0fb2ce>
2012-10-23 18:10:48 +0000

    DeiBogus_Actividade de Login

---
+++ trunk/src/su/android/client/LoginActivity.java
@@ -0,0 +1,66 @@
+package su.android.client;
+
+import su.android.model.Login;
+import su.android.server.connection.ServerConnection;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+	private ServerConnection conn;
+	private Button loginButton;
+	private EditText emailEdit;
+	private EditText passEdit;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+		//server interface
+		conn = new ServerConnection();
+
+		//ligaçoes com elementos do layout
+		loginButton = (Button) findViewById(R.id.btnLogin);
+
+		//handle login button
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				emailEdit = (EditText) findViewById(R.id.emailInput);
+				passEdit = (EditText) findViewById(R.id.passwordlInput);
+				
+				//verify integrity
+				if(emailEdit.getText().toString().equals("") || passEdit.getText().toString().equals("")){
+					String text = "Missing input";
+
+					Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+					toast.show();
+					
+					return;
+
+				}
+				//Verify credentials on DB
+				Login login = new Login(emailEdit.getText().toString(),passEdit.getText().toString());
+				if(conn.verifyLogin(login) == true){
+					// Switching to Main Activity
+					Intent i = new Intent(getApplicationContext(), MainScreen.class);
+					startActivity(i);
+				}
+				else{
+					String text = "Invalid Login";
+
+					Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+					toast.show();
+
+				}
+
+			}
+		});
+	}
+}
